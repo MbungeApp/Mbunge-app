b47196467c4245019e8d357782a223b3a5dcf654
Iampato <858wpwaweru@gmail.com>
2020-04-01 20:48:22 +0300

    ui changes

--- lib/pages/pariticipate/participate_item.dart
+++ lib/pages/pariticipate/participate_item.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/material.dart';
 
 class ParticipationItem extends StatelessWidget {
-  const ParticipationItem({
-    Key key,
-    this.onTap,
-  }) : super(key: key);
+  const ParticipationItem(
+      {Key key, this.onTap, this.title, this.desc, this.postedBy})
+      : super(key: key);
   final Function onTap;
+  final String title;
+  final String desc;
+  final String postedBy;
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: onTap,
-      isThreeLine: true,
-      title: Text("sample title"),
-      subtitle: Text("sample description"),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListTile(
+        onTap: onTap,
+        title: Text(title),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              desc,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                postedBy,
+                style:Theme.of(context).textTheme.caption
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
