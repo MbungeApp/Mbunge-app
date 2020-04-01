3b383132eeb9f6ccf0b99520a763697c92e2fa1f
Iampato <858wpwaweru@gmail.com>
2020-04-01 20:46:35 +0300

    downgrade flutter bloc

--- lib/utils/blocdelegate.dart
+++ lib/utils/blocdelegate.dart
@@ -3,25 +3,19 @@ import 'package:bloc/bloc.dart';
 class SimpleBlocObserver extends BlocObserver {
   @override
   void onEvent(Bloc bloc, Object event) {
-    print(event);
     super.onEvent(bloc, event);
+    print(event);
   }
 
   @override
-  void onChange(Cubit cubit, Change change) {
-    print(change);
-    super.onChange(cubit, change);
-  }
-
-  @override
-  void onTransition(Bloc bloc, Transition transition) {
-    print(transition);
+  onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
+    print(transition);
   }
 
   @override
-  void onError(Cubit cubit, Object error, StackTrace stackTrace) {
+  void onError(Bloc bloc, Object error, StackTrace stacktrace) {
+    super.onError(bloc, error, stacktrace);
     print(error);
-    super.onError(cubit, error, stackTrace);
   }
 }
