2c65569edf39824423ed735244f1a0ed61c423a0
Iampato <858wpwaweru@gmail.com>
2020-04-01 20:48:47 +0300

    wired in bloc

--- lib/pages/events/event.dart
+++ lib/pages/events/event.dart
@@ -1,6 +1,12 @@
 import 'package:animations/animations.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mbunge/blocs/event/event_bloc.dart';
+import 'package:mbunge/models/http/_http.dart';
+import 'package:mbunge/repository/network/event_repository.dart';
+import 'package:mbunge/widgets/shimmer.dart';
+import 'event_detail.dart';
 import 'event_item.dart';
 import 'dart:io' show Platform;
 
@@ -22,11 +28,58 @@ class _EventPageState extends State<EventPage> {
               transitionBetweenRoutes: false,
               middle: Text("Events & News"),
             ),
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, i) {
-                return EventItem();
-              },
+            child: BlocProvider(
+              create: (context) => EventBloc(EventRepository())
+                ..add(
+                  LoadingEvents(),
+                ),
+              child: BlocBuilder<EventBloc, EventState>(
+                builder: (context, state) {
+                  if (state is EventInitial) {
+                    return Center(
+                      child: Shimmer(
+                        gradient: LinearGradient(
+                          colors: [Colors.grey, Colors.white],
+                        ),
+                        child: Text(
+                          "Mbunge",
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                      ),
+                    );
+                  }
+                  if (state is EventsError) {
+                    return Center(
+                      child: Text("Error"),
+                    );
+                  }
+                  if (state is EventsLoaded) {
+                    List<Event> events = state.events;
+                    return ListView.builder(
+                      itemCount: events.length,
+                      itemBuilder: (context, i) {
+                        return EventItem(
+                          name: events[i].name,
+                          body: events[i].body,
+                          created: events[i].createdAt,
+                          image: events[i].picture,
+                          ontap: () {
+                            Navigator.push(
+                              context,
+                              CupertinoPageRoute(
+                                builder: (context) {
+                                  return EventDetail(event:events[i]);
+                                },
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    );
+                  }
+                  return Container();
+                },
+              ),
             ),
           )
         : Scaffold(
