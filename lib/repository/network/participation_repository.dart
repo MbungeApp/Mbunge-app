aaf1279e6c26d1a2a7702a507e0eeecd4f0eb67f
Iampato <858wpwaweru@gmail.com>
2020-04-01 20:47:08 +0300

    added participations repo

--- lib/repository/network/participation_repository.dart
+++ lib/repository/network/participation_repository.dart
@@ -1,3 +1,34 @@
-abstract class ParticipationInterface {}
+import 'dart:convert';
 
-class ParticipationRepository implements ParticipationInterface {}
+import 'package:mbunge/models/http/_http.dart';
+import 'package:mbunge/utils/network/http.dart';
+
+abstract class ParticipationInterface {
+  Future<List<Participation>> getParticipations();
+}
+
+class ParticipationRepository implements ParticipationInterface {
+  static final ParticipationRepository _userRepository =
+      ParticipationRepository._internal();
+  factory ParticipationRepository() {
+    return _userRepository;
+  }
+  ParticipationRepository._internal();
+
+  final HttpClient httpClient = HttpClient();
+  @override
+  Future<List<Participation>> getParticipations() async {
+    final response = await httpClient.getRequest(url: "/participation/");
+    print("REEEEE: ${response.statusCode}");
+    print("REEEEE: ${response.body}");
+    if (response.statusCode != 200) {
+      throw Exception('error logging in');
+    }
+    print("232323");
+    final responseJson = jsonDecode(response.body);
+    print("1111111");
+    return List<Participation>.from(
+      responseJson.map((x) => Participation.fromJson(x)),
+    ); // participationFromJson(responseJson);
+  }
+}
