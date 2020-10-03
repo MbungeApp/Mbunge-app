8da20dcd7c5f9bf6835c205314a035f36f8dba6a
Iampato <858wpwaweru@gmail.com>
2020-10-03 20:13:32 +0300

    A new mp

--- lib/pages/mp/mp.dart
+++ lib/pages/mp/mp.dart
@@ -1,353 +1,122 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:mbunge/pages/mp/widget/more.dart';
+import 'package:mbunge/pages/mp/widget/single.dart';
 
-class MPPage extends StatefulWidget {
+class MpPage extends StatefulWidget {
   @override
-  _MPPageState createState() => _MPPageState();
+  _MpPageState createState() => _MpPageState();
 }
 
-class _MPPageState extends State<MPPage> with SingleTickerProviderStateMixin {
-  AnimationController animationController;
-  Animation<double> heightFactorAnimation;
-  bool animationDone = false;
-  bool expanded = false;
-  double start;
-  double end;
+class _MpPageState extends State<MpPage> {
+  double pageOffset = 0;
+  PageController _pageController = PageController();
+  String direction;
 
   @override
   void initState() {
-    animationController = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 300),
-    );
-    heightFactorAnimation = Tween<double>(begin: 0.85, end: 0.4)
-        .chain(
-          CurveTween(curve: Interval(0.1, 1.0, curve: Curves.linear)),
-        )
-        .animate(animationController);
-    animationController.addStatusListener((current) {
-      if (current == AnimationStatus.completed) {
-        animationDone = true;
-        setState(() {
-          expanded = !expanded;
-        });
-      }
+    _pageController.addListener(() {
+      setState(() {
+        pageOffset = _pageController.page;
+        direction = _listener();
+      });
     });
     super.initState();
   }
 
-  @override
-  void dispose() {
-    animationController.dispose();
-    super.dispose();
-  }
-
-  // on-tap
-  onBottomTap() {
-    setState(() {
-      if (animationDone) {
-        animationController.reverse();
-      } else {
-        animationController.forward();
-      }
-      animationDone = !animationDone;
-    });
-  }
-
-  onBottomDragStart(dragStartDetails) {
-    double deviceHeight = MediaQuery.of(context).size.height;
-    double startPosition = dragStartDetails.globalPosition.dy;
-    double percentageStart =
-        (((startPosition - deviceHeight) / deviceHeight) * 100) * -1;
-    setState(() {
-      start = percentageStart;
-    });
-  }
-
-  onBottomDragUpdate(dragUpdateDetails) {
-    double endPosition = dragUpdateDetails.globalPosition.dy;
-    double deviceHeight = MediaQuery.of(context).size.height;
-    double percentageEnd =
-        (((endPosition - deviceHeight) / deviceHeight) * 100) * -1;
-    setState(() {
-      end = percentageEnd;
-    });
-  }
-
-  onBottomDragEnd(dragEndDetails) {
-    if (end > start) {
-      if ((end - start) > 10) {
-        animationController.forward();
-      }
+  String _listener() {
+    if (_pageController.position.userScrollDirection ==
+        ScrollDirection.reverse) {
+      return "right";
     } else {
-      animationController.reverse();
+      return "left";
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    double currentAnim = pageOffset - pageOffset.toInt();
     return Material(
-      child: AnimatedBuilder(
-        animation: animationController,
-        builder: (context, child) {
-          return Stack(
-            fit: StackFit.expand,
-            children: <Widget>[
-              FractionallySizedBox(
-                alignment: Alignment.topCenter,
-                heightFactor: heightFactorAnimation.value,
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: <Widget>[
-                    _buildImage(),
-                    // User details
-                    Positioned(
-                      bottom: 40.0,
-                      width: MediaQuery.of(context).size.width,
-                      child: _buildUserDetails(),
-                    ),
-                  ],
+      color: CupertinoColors.lightBackgroundGray,
+      child: PageView(
+        controller: _pageController,
+        children: <Widget>[
+          AnimatedBuilder(
+            animation: _pageController,
+            builder: (context, child) {
+              return Padding(
+                padding: EdgeInsets.only(
+                  top: paddingTop1(currentAnim),
+                  right: currentAnim != 0 ? currentAnim * 30 : 0,
                 ),
-              ),
-              Positioned(
-                child: FractionallySizedBox(
-                  alignment: Alignment.bottomCenter,
-                  heightFactor: 1.05 - heightFactorAnimation.value,
-                  child: GestureDetector(
-                    onTap: () => onBottomTap(),
-                    onVerticalDragStart: (dragStartDetails) =>
-                        onBottomDragStart(dragStartDetails),
-                    onVerticalDragUpdate: (dragUpdateDetails) =>
-                        onBottomDragUpdate(dragUpdateDetails),
-                    onVerticalDragEnd: (dragEndDetails) =>
-                        onBottomDragEnd(dragEndDetails),
-                    child: _buildPageDetails(),
-                  ),
-                ),
-              ),
-            ],
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _buildImage() {
-    return Stack(
-      children: <Widget>[
-        Image.asset(
-          "assets/illustrate/04.png",
-          height: double.infinity,
-          width: double.infinity,
-          fit: BoxFit.cover,
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.bottomCenter,
-                end: Alignment.topCenter,
-                colors: [
-                  CupertinoColors.systemOrange,
-                  Colors.transparent,
-                ],
+                child: child,
+              );
+            },
+            child: ClipRRect(
+              borderRadius: currentAnim != 0
+                  ? BorderRadius.circular(10)
+                  : BorderRadius.circular(0),
+              child: SingleMpPage(
+                pageController: _pageController,
               ),
             ),
-            height: MediaQuery.of(context).size.height * 0.32,
           ),
-        )
-      ],
-    );
-  }
-
-  Widget _buildUserDetails() {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 10.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                "12",
-                style: Theme.of(context)
-                    .textTheme
-                    .headline6
-                    .copyWith(color: Colors.white),
-              ),
-              Opacity(
-                opacity: 0.8,
-                child: Text(
-                  "photos",
-                  style: Theme.of(context)
-                      .textTheme
-                      .caption
-                      .copyWith(color: Colors.white),
+          AnimatedBuilder(
+            animation: _pageController,
+            builder: (context, child) {
+              return Padding(
+                padding: EdgeInsets.only(
+                  top: paddingTop2(currentAnim),
                 ),
-              )
-            ],
+                child: child,
+              );
+            },
+            child: ClipRRect(
+              borderRadius: currentAnim != 0
+                  ? BorderRadius.circular(10)
+                  : BorderRadius.circular(0),
+              child: MoreMps(
+                pageController: _pageController,
+              ),
+            ),
           ),
-          Column(
-            children: <Widget>[
-              Text(
-                "20",
-                style: Theme.of(context)
-                    .textTheme
-                    .headline6
-                    .copyWith(color: Colors.white),
-              ),
-              Opacity(
-                opacity: 0.8,
-                child: Text(
-                  "age (years)",
-                  style: Theme.of(context)
-                      .textTheme
-                      .caption
-                      .copyWith(color: Colors.white),
-                ),
-              )
-            ],
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              Text(
-                "23",
-                style: Theme.of(context)
-                    .textTheme
-                    .headline6
-                    .copyWith(color: Colors.white),
-              ),
-              Opacity(
-                opacity: 0.8,
-                child: Text(
-                  "distance (km)",
-                  style: Theme.of(context)
-                      .textTheme
-                      .caption
-                      .copyWith(color: Colors.white),
-                ),
-              )
-            ],
-          )
         ],
       ),
     );
   }
 
-  Widget _buildPageDetails() {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topRight: Radius.circular(30),
-          topLeft: Radius.circular(30),
-        ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "John Mayer",
-                        style: Theme.of(context)
-                            .textTheme
-                            .subhead
-                            .copyWith(fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "Kijabe",
-                        style: Theme.of(context).textTheme.caption,
-                      )
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 10.0),
-                  child: OutlineButton(
-                    borderSide: BorderSide(color: Colors.red, width: 2),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Center(child: Text("more")),
-                    onPressed: () {
-                      //TODO
-                    },
-                  ),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: ListView(
-              physics: expanded
-                  ? BouncingScrollPhysics()
-                  : NeverScrollableScrollPhysics(),
-              // crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 30,
-                    vertical: 10,
-                  ),
-                  child: Text(
-                    "Nostrud amet proident adipisicing sint magna sit Est cillum velit anim culpa nostrud ut velit officia commodo." *
-                        10,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 30.0, bottom: 8.0),
-                  child: Text(
-                    "Photos",
-                    style: Theme.of(context)
-                        .textTheme
-                        .subhead
-                        .copyWith(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 30.0),
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 100,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 5,
-                      itemBuilder: (context, i) {
-                        return Padding(
-                          padding: const EdgeInsets.only(right: 10),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                            child: Image.asset(
-                              "assets/illustrate/0${i + 1}.png",
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+  double paddingTop1(double currentAnim) {
+    if (currentAnim == 0) {
+      return 0;
+    } else {
+      if (direction == "right" && currentAnim <= 0.3) {
+        return currentAnim * 120;
+      } else if (direction == "left" && currentAnim <= 0.5) {
+        if (currentAnim > 0.3) {
+          return 0.3 * 120;
+        } else {
+          return currentAnim * 120;
+        }
+      } else {
+        return 0.3 * 120;
+      }
+    }
+  }
+
+  double paddingTop2(double currentAnim) {
+    if (currentAnim == 0) {
+      return 0;
+    } else {
+      if (direction == "right" && currentAnim <= 0.5) {
+        return 0.3 * 120;
+      } else {
+        if ((1 - currentAnim) > 0.3) {
+          return 0.3 * 120;
+        } else {
+          return (1 - currentAnim) * 120;
+        }
+      }
+    }
   }
 }
