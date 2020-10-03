ca614585ac78fa840e0b6f7d5caf5cafacd03b1d
Iampato <858wpwaweru@gmail.com>
2020-10-03 20:10:31 +0300

    added the participation detail page

--- lib/pages/_pageindex.dart
+++ lib/pages/_pageindex.dart
@@ -3,3 +3,4 @@ export 'package:mbunge/pages/account/confirm_code.dart';
 export 'package:mbunge/pages/navigation/bottom.dart';
 export 'package:mbunge/pages/splashscreen/splashscreen.dart';
 export 'package:mbunge/pages/theme/theme.dart';
+export 'package:mbunge/pages/pariticipate/participate_detail.dart';
---
+++ lib/pages/pariticipate/participate_detail.dart
@@ -0,0 +1,72 @@
+import 'package:animations/animations.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const double _fabDimension = 56.0;
+
+class ParticipationDetail extends StatefulWidget {
+  @override
+  _ParticipationDetailState createState() => _ParticipationDetailState();
+}
+
+class _ParticipationDetailState extends State<ParticipationDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          previousPageTitle: "participations",
+          middle: Text("Sample title"),
+        ),
+        child: Center(child: Text("dfdf")),
+      ),
+      floatingActionButton: OpenContainer(
+        transitionType: ContainerTransitionType.fade,
+        openBuilder: (BuildContext context, VoidCallback _) {
+          return CommentsPage();
+        },
+        closedElevation: 6.0,
+        closedShape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(_fabDimension / 2),
+          ),
+        ),
+        closedColor: Theme.of(context).primaryColor,
+        transitionDuration: Duration(milliseconds: 800),
+        closedBuilder: (BuildContext context, VoidCallback openContainer) {
+          return SizedBox(
+            height: _fabDimension,
+            width: _fabDimension,
+            child: Center(
+              child: Icon(Icons.add),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CommentsPage extends StatefulWidget {
+  @override
+  _CommentsPageState createState() => _CommentsPageState();
+}
+
+class _CommentsPageState extends State<CommentsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        iconTheme: IconThemeData(color:Colors.black)
+      ),
+      body: Column(
+        children: [
+          TextFormField(),
+
+        ],
+      ),
+    );
+  }
+}
