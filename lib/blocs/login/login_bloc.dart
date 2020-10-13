5c303d9c487b7efd863aa752d2ccee56a3441b0b
Iampato <858wpwaweru@gmail.com>
2020-10-14 02:15:37 +0300

    wired in login bloc

--- lib/blocs/login/login_bloc.dart
+++ lib/blocs/login/login_bloc.dart
@@ -2,17 +2,36 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:mbunge/models/http/_http.dart';
+import 'package:mbunge/repository/network/user_repository.dart';
 
 part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc() : super(LoginInitial());
+  final UserRepository userRepository;
+  LoginBloc(this.userRepository) : super(LoginInitial());
 
   @override
   Stream<LoginState> mapEventToState(
     LoginEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is SignInWithPhone) {
+      yield* _mapSignInWithPhoneToState(event.loginRequest);
+    }
+  }
+
+  Stream<LoginState> _mapSignInWithPhoneToState(
+      LoginRequest loginRequest) async* {
+    try {
+      LoginResponse loginResponse =
+          await userRepository.userLogin(loginRequest);
+      print("##################################");
+      print("${loginResponse.token}");
+      print("##################################");
+      yield LoginSuccess(loginResponse.user.firstName);
+    } catch (e) {
+      yield LoginError();
+    }
   }
 }
