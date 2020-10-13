269ddaf6b28d6adde5e874a941755b219f9273ed
Iampato <858wpwaweru@gmail.com>
2020-10-14 01:32:59 +0300

    added a http service

--- lib/utils/network/http.dart
+++ lib/utils/network/http.dart
@@ -1,13 +1,39 @@
 import 'package:http/http.dart' as http;
+import 'package:mbunge/repository/share_preferences.dart';
 
 class HttpClient {
-  static String baseUrl = "http://mbungeapp.tech/v1";
+  static SharePreferenceRepo sharePreferenceRepo = SharePreferenceRepo();
+  static String baseUrl = "http://mbungeapp.tech:5000/api/v1";
+
+  static var header1 = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json'
+  };
+  static Map headers(String token) => {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $token',
+      };
 
   static Future<http.Response> getRequest({
     String url,
   }) async {
     final reqUrl = '$baseUrl/$url';
-    return await http.get(reqUrl);
+    String token = await sharePreferenceRepo.getToken();
+    return await http.get(reqUrl, headers: headers(token));
   }
 
+  static Future<http.Response> postRequest({String url, dynamic body}) async {
+    final reqUrl = '$baseUrl/$url';
+    String token = await sharePreferenceRepo.getToken();
+    return await http.post(reqUrl, headers: headers(token), body: body);
+  }
+
+  static Future<http.Response> deleteRequest({
+    String url,
+  }) async {
+    final reqUrl = '$baseUrl/$url';
+    String token = await sharePreferenceRepo.getToken();
+    return await http.delete(reqUrl, headers: headers(token));
+  }
 }
