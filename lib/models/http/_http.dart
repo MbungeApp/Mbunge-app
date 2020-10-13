33846bc4c46e9371b04935d3f30bf202c096d169
Iampato <858wpwaweru@gmail.com>
2020-10-14 01:34:22 +0300

    added all participation model

---
+++ lib/models/http/_http.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+// auth
+part 'login_request.dart';
+part 'login_response.dart';
+part 'register_request.dart';
+part 'register_response.dart';
+
+// event
+part 'event.dart';
+
+// participation
+part 'all_participation.dart';
---
+++ lib/models/http/all_participation.dart
@@ -0,0 +1,53 @@
+// To parse this JSON data, do
+//
+//     final participation = participationFromJson(jsonString);
+
+part of "_http.dart";
+
+List<Participation> participationFromJson(String str) => List<Participation>.from(json.decode(str).map((x) => Participation.fromJson(x)));
+
+String participationToJson(List<Participation> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Participation {
+    Participation({
+        this.id,
+        this.name,
+        this.sector,
+        this.body,
+        this.postedBy,
+        this.expireAt,
+        this.createdAt,
+        this.updatedAt,
+    });
+
+    String id;
+    String name;
+    String sector;
+    String body;
+    String postedBy;
+    DateTime expireAt;
+    DateTime createdAt;
+    DateTime updatedAt;
+
+    factory Participation.fromJson(Map<String, dynamic> json) => Participation(
+        id: json["id"],
+        name: json["name"],
+        sector: json["sector"],
+        body: json["body"],
+        postedBy: json["posted_by"],
+        expireAt: DateTime.parse(json["expire_at"]),
+        createdAt: DateTime.parse(json["created_at"]),
+        updatedAt: DateTime.parse(json["updated_at"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "sector": sector,
+        "body": body,
+        "posted_by": postedBy,
+        "expire_at": expireAt.toIso8601String(),
+        "created_at": createdAt.toIso8601String(),
+        "updated_at": updatedAt.toIso8601String(),
+    };
+}
