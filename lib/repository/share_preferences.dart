87488ae262f5edcea76be750c4af4af95d9bebee
Iampato <858wpwaweru@gmail.com>
2020-10-14 01:32:45 +0300

    added a share preference service

--- lib/repository/share_preferences.dart
+++ lib/repository/share_preferences.dart
@@ -1,3 +1,20 @@
-class SharePreference{
-  
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class SharePreferenceImpl {
+  Future<bool> saveToken(String token);
+  Future<String> getToken();
+}
+
+class SharePreferenceRepo implements SharePreferenceImpl {
+  @override
+  Future<String> getToken() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token');
+  }
+
+  @override
+  Future<bool> saveToken(String token) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString('token', token);
+  }
 }
