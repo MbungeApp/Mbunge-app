9f4faf85b36385d7337a49eb432bd1f75f3bd8d0
Iampato <858wpwaweru@gmail.com>
2020-10-14 04:03:32 +0300

    saving current user and token

--- lib/blocs/login/login_bloc.dart
+++ lib/blocs/login/login_bloc.dart
@@ -4,13 +4,16 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:mbunge/models/http/_http.dart';
 import 'package:mbunge/repository/network/user_repository.dart';
+import 'package:mbunge/repository/share_preferences.dart';
 
 part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   final UserRepository userRepository;
-  LoginBloc(this.userRepository) : super(LoginInitial());
+  final SharePreferenceRepo sharePreferenceRepo;
+  LoginBloc(this.userRepository, this.sharePreferenceRepo)
+      : super(LoginInitial());
 
   @override
   Stream<LoginState> mapEventToState(
@@ -23,14 +26,23 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
 
   Stream<LoginState> _mapSignInWithPhoneToState(
       LoginRequest loginRequest) async* {
+    yield LoginInitial();
     try {
-      LoginResponse loginResponse =
-          await userRepository.userLogin(loginRequest);
+      LoginResponse loginResponse = await userRepository.userLogin(
+        loginRequest,
+      );
       print("##################################");
       print("${loginResponse.token}");
       print("##################################");
+
+      await sharePreferenceRepo.saveToken(loginResponse.token);
+      await sharePreferenceRepo.saveUser(
+        loginResponse.user.toJson().toString(),
+      );
+
       yield LoginSuccess(loginResponse.user.firstName);
     } catch (e) {
+      print("Exception: $e");
       yield LoginError();
     }
   }
