ecd04c3f60530ff40dd6336351ce2cb90084c69f
Iampato <858wpwaweru@gmail.com>
2020-10-14 04:02:51 +0300

    added bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mbunge/utils/blocdelegate.dart';
 import 'package:mbunge/utils/firebase.dart';
 
 import 'mbunge.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = SimpleBlocObserver();
 
   // setFirebase
   PushNotification pushNotification = PushNotification();
