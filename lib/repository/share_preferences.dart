4130bfe37c45e5b9a57a2e17d69e2c2195572af2
Iampato <858wpwaweru@gmail.com>
2020-10-14 04:04:50 +0300

    added loggged in user and token

--- lib/repository/share_preferences.dart
+++ lib/repository/share_preferences.dart
@@ -1,20 +1,49 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class SharePreferenceImpl {
+  Future<bool> saveNewUser(bool newUser);
+  Future<bool> getNewUser();
   Future<bool> saveToken(String token);
   Future<String> getToken();
+
+  Future<bool> saveUser(String user);
+  Future<String> getUsert();
 }
 
 class SharePreferenceRepo implements SharePreferenceImpl {
   @override
   Future<String> getToken() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getString('token');
+    return prefs.getString('token') ?? "";
   }
 
   @override
   Future<bool> saveToken(String token) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.setString('token', token);
+  }
+
+  @override
+  Future<bool> getNewUser() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('new_user') ?? false;
+  }
+
+  @override
+  Future<bool> saveNewUser(bool newUser) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setBool('new_user', newUser);
+  }
+
+  @override
+  Future<String> getUsert() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString("user") ?? "";
+  }
+
+  @override
+  Future<bool> saveUser(String user) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString("user", user);
   }
 }
